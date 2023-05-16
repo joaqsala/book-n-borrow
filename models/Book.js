9b66079a8df6dd0ba8d2668520727303a6d4ecbd
Joaquin Salazar <joaqsala@gmail.com>
2023-05-15 20:19:15 -0500

    added models and associations in db folder

--- models/Book.js
+++ models/Book.js
@@ -1,14 +1,9 @@
 const { Model, DataTypes } = require('sequelize');
-const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
-class User extends Model {
-  checkPassword(loginPw) {
-    return bcrypt.compareSync(loginPw, this.password);
-  }
-}
+class Book extends Model {}
 
-User.init(
+Book.init(
   {
     id: {
       type: DataTypes.INTEGER,
@@ -16,43 +11,51 @@ User.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    firstName: {
+    bookName: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    lastName: {
+    author: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    email: {
-      type: DataTypes.STRING,
+    yearPublish: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    isbn: {
+      type: DataTypes.INTEGER,
       allowNull: false,
       unique: true,
-      validate: {
-        isEmail: true,
-      },
     },
-    password: {
+    rentalPrice: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+    },
+    category: {
       type: DataTypes.STRING,
       allowNull: false,
-      validate: {
-        len: [8],
+    },
+    bookcoverURL: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    owner_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'user',
+        key: 'id',
       },
     },
   },
   {
-    hooks: {
-      beforeCreate: async (newUserData) => {
-        newUserData.password = await bcrypt.hash(newUserData.password, 10);
-        return newUserData;
-      },
-    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'user',
+    modelName: 'book',
   }
 );
 
-module.exports = User;
+module.exports = Book;
--- models/index.js
+++ models/index.js
@@ -1,3 +1,24 @@
 const User = require('./User');
+const Book = require('./Book');
 
-module.exports = { User };
+//ownership
+User.hasMany(Book, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+    });
+
+Book.belongsTo(User, {
+    foreignKey: 'user_id',
+    });
+
+//rental
+User.hasMany(Book, {
+    through: Renter 
+    });
+
+Book.belongsTo(User, {
+    through: Renter 
+    });
+
+
+module.exports = { User, Book };
