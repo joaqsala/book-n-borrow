6f87c58bc16c8e8b30ad25bbeeeef7dd5ab55291
KodaCaleb <122597981+KodaCaleb@users.noreply.github.com>
2023-05-21 04:38:52 -0500

    mother of god finally

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+const bookRoutes = require('./api/bookRoutes');  // Updated this line
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/api/book', bookRoutes);
 
 module.exports = router;
--- models/User.js
+++ models/User.js
@@ -2,6 +2,7 @@ const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
+
 class User extends Model {
   checkPassword(loginPw) {
     return bcrypt.compareSync(loginPw, this.password);
@@ -54,5 +55,6 @@ User.init(
     modelName: 'user',
   }
 );
+
 
 module.exports = User;
