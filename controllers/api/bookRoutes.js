151f374ea118c42a084ddcad1d5fa29a658f9534
KodaCaleb <122597981+KodaCaleb@users.noreply.github.com>
2023-05-21 16:55:36 -0500

    can now delete your listings

--- controllers/api/bookRoutes.js
+++ controllers/api/bookRoutes.js
@@ -96,6 +96,26 @@ router.put('/return/:rentalId', async (req, res) => {
         res.status(500).json({ message: 'Failed to return book' });
     }
 });
+
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const bookData = await Book.destroy({
+            where: {
+            id: req.params.id,
+            owner_id: req.session.user_id,
+            },
+        });
+    
+        if (!bookData) {
+            res.status(404).json({ message: 'No book found with this id!' });
+            return;
+        }
+    
+        res.status(200).json(bookData);
+        } catch (err) {
+        res.status(500).json(err);
+        }
+    });
     
 
 
